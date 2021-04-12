2b1bf6ade85f84f87fe27e63a292c56f3cede161
matheus695p <mateu_695p@hotmail.com>
2021-04-11 22:49:38 -0400

    add: request

---
+++ apis/weatherstack.py
@@ -0,0 +1,19 @@
+import json
+import requests
+
+# lectura del api key
+with open('keys.json') as json_file:
+    dict_ = json.load(json_file)
+    YOUR_ACCESS_KEY = dict_["weatherstack"]
+    print(YOUR_ACCESS_KEY)
+
+# params del request
+params = {
+    'access_key': YOUR_ACCESS_KEY,
+    'query': 'Santiago'
+}
+# consulta
+api_result = requests.get('http://api.weatherstack.com/current', params)
+# respuesta
+api_response = api_result.json()
+print(api_response)
